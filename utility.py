dc94830252426ae4f3fd8c702e6530d645bf8e34
Brian Quach <13rianquach@gmail.com>
2016-03-16 20:44:29 -0700

    feat: add function to return model from url safe key

---
+++ utility.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+"""
+Copyright 2016 Brian Quach
+Licensed under MIT (https://github.com/brianquach/udacity-nano-fullstack-conference/blob/master/LICENSE)  # noqa
+
+Code Citation:
+  https://github.com/udacity/FSND-P4-Design-A-Game/blob/master/Skeleton%20Project%20Guess-a-Number/utils.py  #noqa
+"""
+import endpoints
+
+from google.appengine.ext import ndb
+
+
+def get_by_urlsafe(urlsafe, model):
+    """Returns an ndb.Model entity that the urlsafe key points to. Checks
+        that the type of entity returned is of the correct kind. Raises an
+        error if the key String is malformed or the entity is of the incorrect
+        kind
+    Args:
+        urlsafe: A urlsafe key string
+        model: The expected entity kind
+    Returns:
+        The entity that the urlsafe Key string points to or None if no entity
+        exists.
+    Raises:
+        ValueError:"""
+    try:
+        key = ndb.Key(urlsafe=urlsafe)
+    except TypeError:
+        raise endpoints.BadRequestException('Invalid Key')
+    except Exception, e:
+        if e.__class__.__name__ == 'ProtocolBufferDecodeError':
+            raise endpoints.BadRequestException('Invalid Key')
+        else:
+            raise
+
+    entity = key.get()
+    if not entity:
+        return None
+    if not isinstance(entity, model):
+        raise ValueError('Incorrect Kind')
+    return entity
