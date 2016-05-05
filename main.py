40687bcf4380ffbb1078e4e5e620dacd7973cc3d
Brian Quach <13rianquach@gmail.com>
2016-05-04 20:25:50 -0700

    feat: email players of the game results

--- main.py
+++ main.py
@@ -75,9 +75,90 @@ move to the server.
             body
         )
 
+class SendGameResultEmail(webapp2.RequestHandler):
+    def post(self):
+        """Send an email to a User that it is their turn"""
+        game = get_by_urlsafe(self.request.get('game_key'), Game)
+
+        player_one_hand = Hand.query(
+            ndb.AND(Hand.player == game.player_one, Hand.game == game.key)
+        ).get()
+        if not player_one_hand:
+            raise endpoints.NotFoundException(
+                'Hand not found for player key {0} and game key {1}'.format(
+                    game.player_one, game.key
+                )
+            )
+
+        player_two_hand = Hand.query(
+            ndb.AND(Hand.player == game.player_one, Hand.game == game.key)
+        ).get()
+        if not player_two_hand:
+            raise endpoints.NotFoundException(
+                'Hand not found for player key {0} and game key {1}'.format(
+                    game.player_two, game.key
+                )
+            )
+        
+        player_one = game.player_one.get()
+        hand = json.loads(player_one_hand.hand)
+        cards = [Card(name=card['name'], suit=card['suit']) for card in hand]
+        p1_hand_information = ''
+        for card in cards:
+            p1_hand_information += 'Card: {0}\n'.format(repr(card))
+
+        player_two = game.player_two.get()
+        hand = json.loads(player_two_hand.hand)
+        cards = [Card(name=card['name'], suit=card['suit']) for card in hand]
+        p2_hand_information = ''
+        for card in cards:
+            p2_hand_information += 'Card: {0}\n'.format(repr(card))
+
+
+        subject = 'It''s a tie!'
+        if game.winner == game.player_one:
+            subject = '{0} Wins!'.format(player_one.name)
+        elif game.winner == game.player_two:
+            subject = '{0} wins!'.format(player_two.name)
+        
+        body = """
+Game finished! {0}
+
+{1}'s hand:
+{2}
+
+{3}'s hand:
+{4}
+        """.format(
+            subject,
+            player_one.name,
+            p1_hand_information,
+            player_two.name,
+            p2_hand_information
+        )
+
+        print body
+        mail.send_mail(
+            'noreply@{}.appspotmail.com'.format(
+                app_identity.get_application_id()
+            ),
+            player_one.email,
+            subject,
+            body
+        )
+        mail.send_mail(
+            'noreply@{}.appspotmail.com'.format(
+                app_identity.get_application_id()
+            ),
+            player_two.email,
+            subject,
+            body
+        )
+
 app = webapp2.WSGIApplication(
     [
-        ('/tasks/send_move_email', SendMoveEmail)
+        ('/tasks/send_move_email', SendMoveEmail),
+        ('/tasks/send_game_result_email', SendGameResultEmail)
     ],
     debug=True
 )
